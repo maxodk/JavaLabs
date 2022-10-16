a8a92be5eaa38d41ef9e0ae41a2daa934810fd6b
maks23438 <42715791+maxodk@users.noreply.github.com>
2022-10-16 20:07:15 +0300

    Delete Boots.java

--- Boots.java
+++
@@ -1,7 +0,0 @@
-package knight;
-
-public class Boots extends Ammunition{
-    public Boots(double cost, double weight, String material, double strength) {
-        super(cost, weight, material, strength);
-    }
-}
