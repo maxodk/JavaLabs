d510e6df27529b98364a81f9cbd492bdfc01497c
maks23438 <42715791+maxodk@users.noreply.github.com>
2022-10-16 20:07:21 +0300

    Delete Breastplate.java

--- Breastplate.java
+++
@@ -1,7 +0,0 @@
-package knight;
-
-public class Breastplate extends Ammunition{
-    public Breastplate(double cost, double weight, String material, double strength) {
-        super(cost, weight, material, strength);
-    }
-}
